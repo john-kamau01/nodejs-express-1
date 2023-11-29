c99706c03e6912843f20d87a8bc59aa2d32d80ab
John Waweru <johnwaweru092@gmail.com>
2023-11-29 15:16:27 +0300

    updated static

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-app.use(express.static("public"));
+app.use("/public", express.static());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
